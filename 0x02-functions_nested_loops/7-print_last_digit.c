d4955523d457e46dc9455a24e2095ccf95b5bbbb
HonMD1 <adetunjimuideen@gmail.com>
2022-12-13 14:04:57 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,22 @@
  */
 int print_last_digit(int n)
 {
+	int a;
+
+	if (n < 0)
+		n = -n
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+		_putchar(a + '0');
+
+		return (a);
+}
+
+{
 	int r;
 
 	print_last_digit(98);
