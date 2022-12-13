d3d6fea5bd97b03a802070cbae29d5b0ce06bd8f
HonMD1 <adetunjimuideen@gmail.com>
2022-12-13 12:43:06 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - print the sign of a number
  * @n: the number to be checked for
- * Returns 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
