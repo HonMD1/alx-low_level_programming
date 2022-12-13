aeba208eeff290ac18b05b39d4d731e213638c79
HonMD1 <adetunjimuideen@gmail.com>
2022-12-13 11:24:02 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for letter = ('a'; letter <= 'z'; letter++)
+		for (letter ='a'; letter <= 'z'; letter++);
 			_putchar(letter);
+		_putchar('\n');
 	}
-	_putchar('\n');
-	return (0);
-	
 }
