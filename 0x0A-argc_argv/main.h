e9dbb144f43dd9ed6c0d09586a030c6e9e5f8eb3
HonMD1 <adetunjimuideen@gmail.com>
2023-01-10 13:11:44 -0800

    update

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,47 +1,6 @@
 #ifndef MAIN_H
-		
 #define MAIN_H
-		
 
-		
 int _putchar(char c);
-		
 int _atoi(char *s);
-		
-
-		
 #endif
-
-
-=========================
-
-
--putchar code
-
-
-#include <stdio.h>
-		
-#include "main.h"
-		
-
-		
-/**
-		
- * _putchar - writes the character c to stdout
-		
- * @c:Ther character to print
-		
- *
-		
- * Return: On success 1.
-		
- * On error, -1 is returned and errno is set appropriately.
-		
- */
-		
-int _putchar(char c)
-{
-
-	return (write(1, &c, 1));
-
-}
